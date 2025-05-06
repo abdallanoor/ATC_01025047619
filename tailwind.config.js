76cff181201a8b3b5142b2d88101f00aa4090398
Abdallah <abdallaahnoor@gmail.com>
2025-05-06 21:15:18 +0300

    build: setup PrimeNG and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+import PrimeUI from 'tailwindcss-primeui';
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: { 
+    extend: {
+      container: {
+        padding: "1rem",
+        center: true,
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "100%",
+          xl: "100%",
+        },
+      },
+      textUnderlineOffset: {
+        6: "6px",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [PrimeUI]
+
+}
