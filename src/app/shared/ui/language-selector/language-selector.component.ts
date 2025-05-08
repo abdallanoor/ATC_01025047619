db906e8d61db9a007f6a04a2d6559e4c259bcf0a
Abdallah <abdallaahnoor@gmail.com>
2025-05-08 22:32:05 +0300

    refactor: navbar

--- src/app/shared/ui/language-selector/language-selector.component.ts
+++ src/app/shared/ui/language-selector/language-selector.component.ts
@@ -27,7 +27,7 @@ interface Language {
       [(ngModel)]="selectedLanguage"
       optionLabel="name"
       (onChange)="onLanguageChange()"
-      class="select w-24 text-xs p-2 ms-1"
+      class="select w-24 text-xs p-2 me-1"
       size="small"
     >
       <ng-template #selectedItem let-selectedOption>
